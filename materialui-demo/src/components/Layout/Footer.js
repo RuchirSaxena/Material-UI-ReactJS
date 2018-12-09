75855a7ee6d86072a31da545aab33e39958966e0
Ruchir <ruchirsaxena@gmail.com>
2018-12-10 01:18:25 +0530

    Usage of : 1.Paper 2.Tabs with Tab

--- materialui-demo/src/components/Layout/Footer.js
+++ materialui-demo/src/components/Layout/Footer.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Paper, Tabs, Tab } from "@material-ui/core";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Paper>
+      <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+        <Tab label="Item 1" />
+        <Tab label="Item2" />
+        <Tab label="Item3" />
+      </Tabs>
+    </Paper>
+  );
 };
 
 export default Footer;
