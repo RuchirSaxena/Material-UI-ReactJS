2639de29a0c9c2b8d05fb022c91d312a1af0aa1e
Ruchir <ruchirsaxena@gmail.com>
2018-12-10 01:40:36 +0530

    Using : 1.Gird 2.Different ways of styling

--- materialui-demo/src/components/Excercises/index.js
+++ materialui-demo/src/components/Excercises/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+
+const style = {
+  Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+};
 
 export default props => {
-  return <div>hello from excercises</div>;
+  return (
+    <Grid container spacing={16} sm={12}>
+      <Grid item sm={6}>
+        <Paper style={style.Paper}>left Panel</Paper>
+      </Grid>
+      <Grid item sm={6}>
+        <Paper style={{ padding: 20, marginTop: 10, marginBottom: 10 }}>
+          Right Panel
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
