bafc4d4bd3d144e0942fab3a19b3222864d04a1f
Ruchir <ruchirsaxena@gmail.com>
2018-12-10 02:06:17 +0530

    Dynamically generating tabs

--- materialui-demo/src/components/Layout/Footer.js
+++ materialui-demo/src/components/Layout/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Paper, Tabs, Tab } from "@material-ui/core";
-const Footer = () => {
+const Footer = ({ muscles }) => {
+  const muslcesData = muscles.map((group, i) => {
+    return <Tab key={i} label={group} />;
+  });
   return (
     <Paper>
       <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
-        <Tab label="Item 1" />
-        <Tab label="Item2" />
-        <Tab label="Item3" />
+        <Tab label="All" />
+        {muslcesData}
       </Tabs>
     </Paper>
   );
